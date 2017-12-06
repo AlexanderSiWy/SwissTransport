2660ffd60944579ae0301f180c1291c19040c11f
Alexander Wyss <alexsilvan.wyss@gmail.com>
2017-12-06 07:47:38 +0100

    getters

--- SwissTransport/main/java/src/transport/Prognosis.java
+++ SwissTransport/main/java/src/transport/Prognosis.java
@@ -6,4 +6,24 @@ public class Prognosis {
 	private String departure;
 	private String capacity1st;
 	private String capacity2nd;
+
+	public String getPlatfrom() {
+		return platfrom;
+	}
+
+	public String getArrival() {
+		return arrival;
+	}
+
+	public String getDeparture() {
+		return departure;
+	}
+
+	public String getCapacity1st() {
+		return capacity1st;
+	}
+
+	public String getCapacity2nd() {
+		return capacity2nd;
+	}
 }
--- SwissTransport/main/java/src/transport/Stop.java
+++ SwissTransport/main/java/src/transport/Stop.java
@@ -11,4 +11,44 @@ public class Stop {
 	private Prognosis prognosis;
 	private String realtimeAvailability;
 	private Location location;
+
+	public Location getStation() {
+		return station;
+	}
+
+	public String getArrival() {
+		return arrival;
+	}
+
+	public String getArrivalTimestamp() {
+		return arrivalTimestamp;
+	}
+
+	public String getDeparture() {
+		return departure;
+	}
+
+	public String getDepartureTimestamp() {
+		return departureTimestamp;
+	}
+
+	public String getDelay() {
+		return delay;
+	}
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public Prognosis getPrognosis() {
+		return prognosis;
+	}
+
+	public String getRealtimeAvailability() {
+		return realtimeAvailability;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
 }
